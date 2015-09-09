e5ca3ee5086e06e810cc66a9262f083b3d0a9859
Chris Olstrom <chris@olstrom.com>
2015-09-09 12:09:32 -0700

    Add spec/*

---
+++ spec/rakuna/session/redis_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe 'RakunaSessionRedis' do
+  it 'fails' do
+    fail 'No tests written. Come on dude, you know better.'
+  end
+end
